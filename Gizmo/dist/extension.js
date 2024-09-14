225dbbba70394052ec7b5dbab9ba66f268694624
branden.immerzeel@gmail.com <branden.immerzeel@gmail.com>
2024-09-14 15:09:29 +0930

    Extension working with compiler

--- Gizmo/dist/extension.js
+++ Gizmo/dist/extension.js
@@ -31,35 +31,18 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.activate = activate;
 exports.deactivate = deactivate;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(__webpack_require__(1));
 const cp = __importStar(__webpack_require__(2));
 const path = __importStar(__webpack_require__(3));
-function isString(value) {
-    return typeof value === "string";
-}
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "Gizmo" is now active!');
     const executablePath = path.join(__dirname, '../Debug/net8.0/Gizmo.exe');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     const disposable = vscode.commands.registerCommand('Gizmo.Compile', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from Gizmo!');
         const workspaceRoot = vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath;
         const gizmoProjectFilePath = workspaceRoot + "\\project.gizmo";
-        vscode.window.showInformationMessage(gizmoProjectFilePath);
+        vscode.window.showInformationMessage("project file path: " + gizmoProjectFilePath);
         const childProcess = cp.spawn(executablePath, [gizmoProjectFilePath]);
         childProcess.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`);
-            // Handle the output from the C++ executable as needed
             vscode.window.showInformationMessage('Compilation output: ' + data.toString());
         });
         childProcess.stderr.on('data', (data) => {
@@ -71,6 +54,10 @@ function activate(context) {
             console.log(`child process exited with code ${code}`);
             if (code === 0) {
                 vscode.window.showInformationMessage('Compilation successful!');
+                const terminal = vscode.window.createTerminal('My Terminal');
+                terminal.sendText('g++ cppProject/main.cpp -o cppProject/build/main.exe');
+                terminal.sendText('start cppProject/build/main.exe');
+                terminal.show();
             }
             else {
                 vscode.window.showErrorMessage('Compilation failed.');
--- Gizmo/src/extension.ts
+++ Gizmo/src/extension.ts
@@ -1,39 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as path from 'path';
 
-function isString(value: unknown): value is string {
-    return typeof value === "string";
-}
-  
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "Gizmo" is now active!');
-
+export function activate(context: vscode.ExtensionContext) 
+{
 	const executablePath = path.join(__dirname, '../Debug/net8.0/Gizmo.exe');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('Gizmo.Compile', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Gizmo!');
 
         const workspaceRoot = vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath;
 		const gizmoProjectFilePath = workspaceRoot + "\\project.gizmo";
-        vscode.window.showInformationMessage(gizmoProjectFilePath);
+        vscode.window.showInformationMessage("project file path: " + gizmoProjectFilePath);
         const childProcess = cp.spawn(executablePath, [gizmoProjectFilePath]);
 
         childProcess.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`);
-            // Handle the output from the C++ executable as needed
             vscode.window.showInformationMessage('Compilation output: ' + data.toString());
         });
 
@@ -51,6 +32,11 @@ export function activate(context: vscode.ExtensionContext) {
             if (code === 0) 
             {
                 vscode.window.showInformationMessage('Compilation successful!');
+
+                const terminal = vscode.window.createTerminal('My Terminal');
+                terminal.sendText('g++ cppProject/main.cpp -o cppProject/build/main.exe');
+                terminal.sendText('start cppProject/build/main.exe');
+                terminal.show();
             } 
             else 
             {
